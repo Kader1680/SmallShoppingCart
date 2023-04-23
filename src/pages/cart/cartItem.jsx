09750fc80a6197b2c2e8289b19472896d0db0d61
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-04-23 18:37:36 +0100

    add new style

--- src/pages/cart/cartItem.jsx
+++ src/pages/cart/cartItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './cartitem.css'
 export default function CartItem(props) {
     const {id, title, img, price} = props.data
     return (
-      <div className='feild'>
+      <div className='card '>
             <img src ={img}  />
             <div className='info'>
                 <p>{title}</p>
