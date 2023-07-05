c6598122e42f86c8199611447692cbb7c6ca604c
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-07-05 02:37:32 +0100

    add

--- src/comenents/navbar.jsx
+++ src/comenents/navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
     <nav className='text-white p-4 d-flex justify-content-around  align-items-center'>
             
             <h4>
-              <Link class="text-white text-decoration-none" to="/">shopping cart</Link>
+              <Link class="text-white text-decoration-none" to="/SmallShoppingCart">shopping cart</Link>
             </h4>
             
-            <Link to="/cart"><i class="text-white text-decoration-none fa-solid fa-cart-shopping fs-4"></i></Link>
+            <Link to="/SmallShoppingCart/cart"><i class="text-white text-decoration-none fa-solid fa-cart-shopping fs-4"></i></Link>
     </nav>
   )
 }
